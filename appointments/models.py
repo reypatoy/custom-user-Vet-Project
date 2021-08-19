80b50c8b3649b1f9a3264ec1fda5388a3b0b8e2c
reypatoy <85288525+reypatoy@users.noreply.github.com>
2021-08-19 14:35:12 +0800

    Creating appointment model in appointment app.

---
+++ appointments/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+from accounts.models import customer
+
+
+class Appointment(models.Model):
+    id = models.IntegerField(primary_key=True)
+    description = models.TextField()
+    date = models.DateTimeField()
+    customer = models.ForeignKey(
+        customer, related_name="appointment", on_delete=models.CASCADE
+    )
+    created_at = models.DateField(auto_now_add=True)
+    status_choices = ((1, "Approve"), (2, "Declined"))
+    status = models.IntegerField(choices=status_choices, null=True, blank=True)
