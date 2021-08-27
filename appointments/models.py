275f775ed9ea471432f164631e7d85f0db0e1580
reypatoy <85288525+reypatoy@users.noreply.github.com>
2021-08-27 22:03:22 +0800

    Initial for customizing appointment list template in doctors dashboard.

--- appointments/models.py
+++ appointments/models.py
@@ -8,9 +8,10 @@ class Appointment(models.Model):
     id = models.IntegerField(primary_key=True)
     description = models.TextField()
     schedule = models.DateTimeField(null=True)
+    old_schedule = models.DateTimeField(null=True)
     customer = models.ForeignKey(
         customer, related_name="appointment", on_delete=models.CASCADE
     )
     created_at = models.DateField(auto_now_add=True)
-    status_choices = ((1, "Approve"), (2, "Declined"))
+    status_choices = ((1, "Approve"), (2, "ReScheduled"), (3, "Declined"))
     status = models.IntegerField(choices=status_choices, null=True, blank=True)
